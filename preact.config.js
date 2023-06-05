2c31199edd605055090ca0996f9f65d4441af06d
Magnus Haugsand <haugsand@gmail.com>
2023-06-05 08:44:54 +0200

    updated webpack config

--- preact.config.js
+++ preact.config.js
@@ -3,5 +3,6 @@ export default {
 	webpack(config, env, helpers, options) {
 		const [ css ] = helpers.getLoadersByName(config, 'css-loader');
 		css.loader.options.modules = false;
+		config.output.hashFunction = 'sha512';
 	}
 };
